a21dc2f1a2673ee6e088d5d515f3132b4074d0d0
Luan Barbosa Ramalho <luan.b.ramalho@gmail.com>
2021-04-10 02:45:03 -0300

    Initial Commit

---
+++ src/test/java/br/com/lbr/heroes/HeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lbr.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
